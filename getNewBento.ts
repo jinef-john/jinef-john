9654d1892ca723222222bef10812331bcac97e31
Jinef <jinefjohn@gmail.com>
2026-02-10 06:01:43 +0300

    Update API URL

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,4 +1,5 @@
-const apiUrl = "https://opbento.vercel.app/api/bento?n=Tuhin%20Mondal&g=Ecolash&x=Tuhin_011&l=tuhin018&i=https%3A%2F%2Fraw.githubusercontent.com%2FEcolash%2FEcolash%2Frefs%2Fheads%2Fmain%2FImages%2Fbanner-pic.png&p=&z=2666d";
+const apiUrl =
+  "https://opbento.vercel.app/api/bento?n=Jinef&i=https%3A%2F%2Fnews.mit.edu%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fnews_article__image_gallery%2Fpublic%2Fimages%2F202012%2FMIT-Coding-Brain-01-press_0.jpg%3Fitok%3DJKoUflf8&g=jinef-john&x=jinefjohn&l=&p=https%3A%2F%2Fjinef.netlify.app%2F&z=7708f";
 interface BentoResponse {
   url: string;
 }
@@ -10,6 +11,7 @@ const fetchBentoUrl = async (apiUrl: string): Promise<string> => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data: BentoResponse = (await response.json()) as BentoResponse;
+    console.log("Fetched Bento URL:", data.url);
     return data.url;
   } catch (error) {
     console.error("Error fetching Bento URL:", error);
